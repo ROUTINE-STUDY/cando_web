9cb9187808af07a8e2354ffa2f262f9eaf132a03
JaeHong94 <moonshin123@naver.com>
2021-09-05 18:37:35 +0900

    Add: about page

--- src/router/About.vue
+++ src/router/About.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>About</h1>
+  <section>
+
+  </section>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+section {
+  width: 340px;
+  padding: 30px;
+  background-color: #fff;
+  margin: 50px auto;
+  box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+}
+</style>

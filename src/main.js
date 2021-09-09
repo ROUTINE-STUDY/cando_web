b872dae0ac8e8c832f4cfaabbcf34ffa1867cf50
JaeHong94 <moonshin123@naver.com>
2021-09-09 23:02:22 +0900

    Refactor: login page

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+// fontawesome
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';

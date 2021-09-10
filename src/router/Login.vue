42351804202f7bf21e371b564779f08c5060d3ad
JaeHong94 <moonshin123@naver.com>
2021-09-10 17:59:13 +0900

    Refactor: Login page Input code change.

--- src/router/Login.vue
+++ src/router/Login.vue
@@ -2,8 +2,14 @@
   <div class="container">
     <div class="inner">
       <h2>Login</h2>
-      <input type="text" placeholder="아이디" />
-      <input type="password" placeholder="비밀번호" />
+      <div class="form-floating mb-3">
+        <input type="text" class="form-control" id="floatingInput" placeholder="아이디">
+        <label for="floatingInput">아이디</label>
+      </div>
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="비밀번호">
+        <label for="floatingPassword">비밀번호</label>
+      </div>
       <button class="btn btn-secondary">로그인</button>
       <button class="btn btn-secondary">아이디 / 비밀번호 찾기</button>
     </div>
@@ -36,7 +42,7 @@ export default {
       border-color: transparent;
       border-bottom: 1px solid $gray-500;
       box-sizing: border-box;
-      &:focus {
+      &.form-control:focus {
         outline: transparent;
       }
     }
